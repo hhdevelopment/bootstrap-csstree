a20d51e69f87a8d4908d2756973a748a9150dbe4
Francois Achache <francois.achache@gmail.com>
2017-03-25 16:57:31 +0100

    improve build

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,36 @@ var gulp = require('gulp'),
 		  del = require('del'),
 		  postcss = require('gulp-postcss'),
 		  cssnano = require('gulp-cssnano'),
-		  mergecss = require('postcss-merge-selectors')
+		  concat = require('gulp-concat'),
+		  mergecss = require('postcss-merge-selectors'),
 		  path = require('path');
 
 gulp.task('default', ['clean', 'build', 'site']);
 
 gulp.task('build', function () {
-	return gulp.src('src/less/tree.less')
+	return gulp.src('src/tree.less')
 	  .pipe(less({paths: [path.join(__dirname, 'less', 'includes')]}))
 	  .pipe(postcss([mergecss()]))
 	  .pipe(cssnano())
 	  .pipe(gulp.dest('dist')).pipe(gulp.dest('public_html'));
 });
 gulp.task('site', function () {
-	return gulp.src(['!src/less', '!src/less/*', 'dist/**/*', 'src/**/*']).pipe(gulp.dest('public_html'));
+	gulp.src([	"src/index.js", "src/index.html", "src/folders.json", "src/tree-template.html"]).pipe(gulp.dest('public_html'));
+	gulp.src([	"node_modules/bootstrap/dist/css/bootstrap.css",
+					"node_modules/bootstrap/dist/css/bootstrap-theme.css"
+				]).pipe(cssnano()).pipe(concat("vendors.css")).pipe(gulp.dest('public_html/css'));
+	gulp.src([	"node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.eot",
+					"node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.svg",
+					"node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf",
+					"node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.woff",
+					"node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2"
+				]).pipe(gulp.dest('public_html/fonts'));
+	return gulp.src([	"node_modules/jquery/dist/jquery.min.js",
+					"node_modules/angular/angular.min.js",
+					"node_modules/bootstrap/dist/js/bootstrap.min.js"
+				]).pipe(concat("vendors.js")).pipe(gulp.dest('public_html/js'));
 });
 
 gulp.task('clean', function () {
-	return del.sync(['dist/**/*', 'public_html/**/*']);
+	return del.sync(['dist/**/*']);
 });
